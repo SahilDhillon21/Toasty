034bad1e288f35b05f41f8a07f55c522d6bc3596
Sahil Dhillon <sodhillon_b21@it.vjti.ac.in>
2025-11-30 11:34:32 +0530

    Setup aibot app

--- toasty/settings.py
+++ toasty/settings.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -22,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "aibot",
 ]
 
 MIDDLEWARE = [
