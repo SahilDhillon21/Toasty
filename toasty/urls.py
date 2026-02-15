ee8ea300061ea19cff167f07f0461974e1387b9a
Sahil Dhillon <sodhillon_b21@it.vjti.ac.in>
2026-02-15 18:18:19 +0530

    Wire the url to correct path

--- toasty/urls.py
+++ toasty/urls.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("aibot/", include("aibot.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("aibot.urls"))]
