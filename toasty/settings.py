2435a94173951aba585cef7ca87c532cc873ab2c
Sahil Dhillon <sodhillon_b21@it.vjti.ac.in>
2026-02-15 14:47:36 +0530

    Update service name and add postgres+celery config in settings

--- toasty/settings.py
+++ toasty/settings.py
@@ -59,10 +59,17 @@ WSGI_APPLICATION = "toasty.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "postgres",  # msut match with the docker service name
+        "PORT": "5432",
     }
 }
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 
 # Password validation
